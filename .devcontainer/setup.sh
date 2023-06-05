ece531dfecf0cd25ba0d9db78caf86df04130b79
Nick Youngblut <nyoungb2@gmail.com>
2023-06-05 17:54:22 +0000

    plotly working

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# conda env install
-#micromamba env create -y -f conda_env.yaml -n conda_env.yaml
-#micromamba install -y python=3.9 pip jupyter
-
-# sam cli install via pip
-pip install --upgrade aws-sam-cli
+# install polars via pip
+pip install --upgrade polars
